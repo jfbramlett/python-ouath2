96b58f94b329170e0d00d223bbf7b304e2f05e96
Bramlett, John <john.bramlett@hbo.com>
2017-11-21 12:00:13 -0500

    Making generic

--- sample-oauth.py
+++ sample-oauth.py
@@ -2,15 +2,18 @@ from oauthlib.oauth2 import BackendApplicationClient
 from requests.auth import HTTPBasicAuth
 from requests_oauthlib import OAuth2Session
 
-client_id = 'herd-client'
-client_secret = 'PkJsgOiBo71'
+client_id = 'given-client-id'
+client_secret = 'given-password'
+token_url = 'https://myauthserver/tokens'
 
 auth = HTTPBasicAuth(client_id, client_secret)
 client = BackendApplicationClient(client_id=client_id)
 oauth = OAuth2Session(client=client)
-token = oauth.fetch_token(token_url='https://oauthdev.homebox.com/auth/v1/tokens', auth=auth)
+token = oauth.fetch_token(token_url=token_url, auth=auth)
 
 
-r = oauth.get('https://herddev-api.homebox.com/reference-data/v1/colors')
+secured_resource = 'https://my-secured-resource'
+
+r = oauth.get(secured_resource)
 
 print r.json()
